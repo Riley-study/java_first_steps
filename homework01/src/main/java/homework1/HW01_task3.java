1048ae3e6bd741ff7af076eb59192dda7e22783b
Riley-study <120293475+Riley-study@users.noreply.github.com>
2023-04-16 09:43:39 +0500

    task 3 solved

--- homework01/src/main/java/homework1/HW01_task3.java
+++ homework01/src/main/java/homework1/HW01_task3.java
@@ -6,6 +6,21 @@ public class HW01_task3 {
         // цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно).
         // Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны,
         // то есть [0][0], [1][1], [2][2], …, [n][n];
-
+        int[][] array = new int[7][7];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (i == j){
+                    array[i][j] = 1;
+                } else if (i+j+1 == array.length) {
+                    array[i][j] = 1;
+                    }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + "  ");
+            }
+            System.out.print("\n");
+        }
     }
 }
