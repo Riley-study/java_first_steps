19c86376ccb5f4541d0cc688d53c19f5efcd4c03
Riley-study <120293475+Riley-study@users.noreply.github.com>
2023-04-29 17:29:41 +0500

    task 1 solved_

--- homework04/src/main/java/HW04_task1.java
+++ homework04/src/main/java/HW04_task1.java
@@ -8,7 +8,7 @@ public class HW04_task1 {
 //    Принимает от пользователя и “запоминает” строки.
 //    Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
 //    Если введено revert, удаляет предыдущую введенную строку из памяти.
-//
+
     private static void ex1() {
         Scanner scanner = new Scanner(System.in);
         Deque<String> wordsListCollection = new ArrayDeque<String>();
@@ -34,6 +34,5 @@ public class HW04_task1 {
         for (String elm: wordsList) {
             System.out.print("[" + elm + "] ");
         }
-
     }
 }
