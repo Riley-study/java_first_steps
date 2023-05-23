737c652a95fe460672c999272a379d53cca1d9fe
Riley-study <120293475+Riley-study@users.noreply.github.com>
2023-05-23 11:24:14 +0500

    add method userAnswerForSecondRequest

--- homework6/src/main/java/mainForNotebookTask.java
+++ homework6/src/main/java/mainForNotebookTask.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class mainForNotebookTask {
     public static void main(String[] args) {
@@ -11,11 +9,11 @@ public class mainForNotebookTask {
                 "black");
         Notebook note2 = new Notebook(2, "Windows", 16, 512,
                 "white");
-        Notebook note3 = new Notebook(3, "IOS", 16, 512,
+        Notebook note3 = new Notebook(3, "IOS", 32, 1024,
                 "white");
         Notebook note4 = new Notebook(4, "IOS", 8, 512,
                 "black");
-        Notebook note5 = new Notebook(5, "Linux", 8, 512,
+        Notebook note5 = new Notebook(5, "Linux", 32, 1024,
                 "white");
         Notebook note6 = new Notebook(6, "Linux", 16, 512,
                 "black");
@@ -26,16 +24,56 @@ public class mainForNotebookTask {
         notebooksCatalog.add(note5);
         notebooksCatalog.add(note6);
         System.out.println(notebooksCatalog.toString());
-        int UserChoice = Integer.parseInt(menuToFilterNotebookCatalog());
 
-        // далее сделать сортирвку
+        int userAnswerForRequest = Integer.parseInt(menuToFilterNotebookCatalog());
+        System.out.println(userAnswerForRequest);
 
+        List<Notebook> selected = new ArrayList<>();
 
+        // Отрабатываем пункты меню:
+        if (userAnswerForRequest == 1) {
+            String scannerRAM = userAnswerForSecondRequest("Введите минимальное значение оперативной памяти: ");
+            int userMinRAM = Integer.parseInt(scannerRAM);
+            for (Notebook note : notebooksCatalog) {
+                if (note.randomAccessMemory >= userMinRAM) {
+                    selected.add(note);
+                }
+            }
+        }
+        if (userAnswerForRequest == 2) {
+            String scannerHDC = userAnswerForSecondRequest("Введите минимальный объем памяти жесткого диска: ");
+            int userMinHDC = Integer.parseInt(scannerHDC);
+            for (Notebook note : notebooksCatalog) {
+                if (note.hardDriveCapacity >= userMinHDC) {
+                    selected.add(note);
+                }
+            }
+        }
+        if (userAnswerForRequest == 3) {
+            String scannerOS = userAnswerForSecondRequest("Введите название операционной системы: ");
+            for (Notebook note : notebooksCatalog) {
+                if (note.operatingSystem.contains(scannerOS)) {
+                    selected.add(note);
+                }
+            }
+        }
+        if (userAnswerForRequest == 4) {
+            String scannerColor = userAnswerForSecondRequest("Введите цвет корпуса (white/black): ");
+            for (Notebook note : notebooksCatalog) {
+                if (note.color.contains(scannerColor)) {
+                    selected.add(note);
+                }
+            }
+        }
+        if (userAnswerForRequest == 5) {
+            System.out.println("Всего хорошего, до новых встреч!");
+        }
+        System.out.println(selected);
     }
 
     public static String menuToFilterNotebookCatalog() {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("Введите число, соответствующее необходимому критерию: \n" +
+        System.out.println("Введите число, соответствующее необходимому критерию для сортировки ноутбуков: \n" +
                 "1 - Размер оперативной памяти\n" +
                 "2 - Объём памяти жесткого диска\n" +
                 "3 - Операционная система\n" +
@@ -52,6 +90,7 @@ public class mainForNotebookTask {
                     System.out.println("Введено некорректное число");
                     menuToFilterNotebookCatalog();
                 }
+                start = false;
 
             } catch (NumberFormatException ex) {
                 System.out.println("Введено некорректное число.");
@@ -59,6 +98,13 @@ public class mainForNotebookTask {
             }
         }
         return scannerNum;
+    }
+
+    public static String userAnswerForSecondRequest(String message){
+        System.out.println(message);
+        Scanner myScanner1 = new Scanner(System.in);
+        String scanner = myScanner1.next();
+        return scanner;
     }
 }
 
